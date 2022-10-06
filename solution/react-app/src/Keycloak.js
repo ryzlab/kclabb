f44bbed6484544beac2dfe36ba035608ec3f162d
Mats Johannesson <ryzlab@ryz.se>
2022-10-06 08:51:44 +0200

    Removed

--- solution/react-app/src/Keycloak.js
+++
@@ -1,9 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloak = new Keycloak({
- url: "http://localhost:8180",
- realm: "kclabbrealm",
- clientId: "reactclient",
-});
-
-export default keycloak;
