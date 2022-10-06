bf85c840bc4e5dbb31ee91ed2e623ca4b9499c99
Mats Johannesson <ryzlab@ryz.se>
2022-10-06 08:53:10 +0200

    Initial revision

---
+++ solution/kclabb/src/main/java/se/ryz/kclabb/KcLabbApplication.java
@@ -0,0 +1,13 @@
+package se.ryz.kclabb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcLabbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcLabbApplication.class, args);
+	}
+
+}
---
+++ solution/kclabb/src/test/java/se/ryz/kclabb/KcLabbApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ryz.kclabb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcLabbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
