fd571a9e50da722e3f3f357c7ff585f82cce842e
Mats Johannesson <ryzlab@ryz.se>
2022-10-10 08:50:56 +0200

    Cleaned up

--- solution/kclabb/src/main/java/se/ryz/kclabb/PingController.java
+++ solution/kclabb/src/main/java/se/ryz/kclabb/PingController.java
@@ -1,9 +1,7 @@
 package se.ryz.kclabb;
 
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +9,10 @@ import java.security.Principal;
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping
 public class PingController {
     private static final Logger logger = Logger.getLogger(PingController.class.getName());
 
     @GetMapping("/ping")
-    @CrossOrigin(origins = "http://localhost:8080")
     @Secured("ROLE_testrole")
     ReplyMessage ping(Principal principal, HttpServletResponse response) {
         logger.info("");
